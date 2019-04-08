ac1b4591e5fb5cfd8ab63b16ac63117850b64d6a
katchip <katchip@users.noreply.github.com>
2019-04-08 14:05:37 -0700

    tested gold labels, added util file

---
+++ util_virushost.py
@@ -0,0 +1,32 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+from builtins import *
+
+import pandas as pd
+from snorkel.models import StableLabel
+from snorkel.db_helpers import reload_annotator_labels
+
+FPATH = r'gold_virushost_labels.txt'
+
+def load_external_labels(session, candidate_class, annotator_name='gold'):
+    gold_labels = pd.read_csv(FPATH, sep="\t")
+    for index, row in gold_labels.iterrows():    
+
+        # We check if the label already exists, in case this cell was already executed
+        context_stable_ids = "~~".join([row['virus'], row['host']])
+        query = session.query(StableLabel).filter(StableLabel.context_stable_ids == context_stable_ids)
+        query = query.filter(StableLabel.annotator_name == annotator_name)
+        if query.count() == 0:
+            session.add(StableLabel(
+                context_stable_ids=context_stable_ids,
+                annotator_name=annotator_name,
+                value=row['label']))            
+    # Commit session
+    session.commit()
+
+    # Reload annotator labels
+    reload_annotator_labels(session, candidate_class, annotator_name, split=0, filter_label_split=False)
+#    reload_annotator_labels(session, candidate_class, annotator_name, split=2, filter_label_split=False)
+    
